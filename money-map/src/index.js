34095008226b5086e6e6f204a52e33173eeb8aff
Dhriti Kumawat <83458440+dhritikumawat@users.noreply.github.com>
2025-08-31 09:53:59 +0000

    refactored setup

--- money-map/src/index.js
+++ money-map/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client'; // use 'react-dom/client' in React 18
 import 'tailwindcss/tailwind.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 // Create root using the new React 18 API
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -11,6 +10,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// Service worker (optional)
-serviceWorker.unregister();
